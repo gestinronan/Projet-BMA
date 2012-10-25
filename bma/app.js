0819b76c69083ba2d15400ed4fa6bb80b3482772
glefloch <glfloch@gmail.com>
2012-10-25 11:45:29 +0200

    connection star

--- bma/app.js
+++ bma/app.js
@@ -1,16 +1,25 @@
 
 /**
- * Module dependencies.
+ * Fichier Serveur projet BMA
  */
 
+// Variable nécéssaire a la création du serveur
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
+// Jquery variable
+var $ = require('jQuery');
+
+// Variable de contact pour le site de la star
+var api_star = http.createClient(80, "http://data.keolis-rennes.com/json/");
+var key_star = "FR6UMKCXT1TY5GJ";
+
 var app = express();
 
+// Configuration du serveur 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -33,6 +42,27 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+//// HTTP Request vers le site de la star /////
+
+// TEST: Creation de l'url et appel ajax
+var url = "http://data.keolis-rennes.com/json/?version=2.0&key=FR6UMKCXT1TY5GJ&cmd=getbikestations";
+getData(url);
+    
+// Création du serveur 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+
+// Get Ajax call function
+function getData(url){
+	
+	// Appel vers le serveur
+	$.get(url, function(data){
+		
+		//success call
+		console.log(data);
+		response = data["data"];
+		console.log(response);
+	})
+}
---
+++ bma/readme.txt
@@ -0,0 +1,9 @@
+/// README
+
+Démarrer le serveur : node app.js
+
+Atteindre le serveur: http://localhost:3000
+
+
+Dans le dossier view: se trouve tous les template. 
+Dans le dossier public: se trouveront toutes les images et fichier static en cas de création de site.
