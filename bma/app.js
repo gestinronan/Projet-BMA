5723d2f5dded5e5daaa14e66d525e1650fac02ba
glefloch <glfloch@gmail.com>
2012-10-31 14:41:16 +0100

    Parse the Json

--- bma/app.js
+++ bma/app.js
@@ -3,6 +3,8 @@
  * Fichier Serveur projet BMA
  */
 
+/********* Variable utilisé par le serveur *********/
+
 // Variable nécéssaire a la création du serveur
 var express = require('express')
   , routes = require('./routes')
@@ -13,12 +15,18 @@ var express = require('express')
 // Jquery variable
 var $ = require('jQuery');
 
+var app = express();
+
+/********* Définition des API que l'on contact *******/
+
 // Variable de contact pour le site de la star
 var api_star = http.createClient(80, "http://data.keolis-rennes.com/json/");
 var version = "2.0";
 var key_star = "FR6UMKCXT1TY5GJ";
 
-var app = express();
+
+
+/********* Configuration du serveur ***********/
 
 // Configuration du serveur 
 app.configure(function(){
@@ -40,31 +48,43 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-// This is the function which manage a get call on '/' (That's the first page which is load)
-app.get('/', function(req, res){
-	
-	// This Select a template and fill it with the parameters
-	res.render('index', { // Index is the template which will be select
-				
-		// Here you add as parameter all the data that you want to put in the template (only for HTML usecases)
-	});
-
-});
-
-
-app.get('/users', user.list);
-
-//// HTTP Request vers le site de la star /////
-
 // TEST: Creation de l'url et appel ajax
 var url = "http://data.keolis-rennes.com/json/?version=" + version + "&key=" + key_star + "&cmd=getbikestations";
 getData(url);
 
-// Création du serveur 
+/************* Get Request ********/
+
+// This is the function which manage a get call on '/' (That's the first page which is load)
+app.get('/', getData(url),function(req, res){
+	res.render('index', {
+		data: req.opendata
+	})
+});
+
+
+/******** Post request **********/
+
+
+// This is a post request (What the phone or the website will send us)
+app.post('/android', function(req, res){
+	
+	// We first get the parameter sent in the request
+	lng = req.body.lng; // Here we get the longitude
+	lat = req.body.lat; // Here we get the latitude
+	
+	// Then we send what we want
+});
+
+
+
+/******* Création du serveur ********/
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
+
+/********* Functions ***********/
 
 // Get Ajax call function
 function getData(url){
@@ -78,7 +98,10 @@ function getData(url){
 		success: function(data){
 			
 			// Function call in case of success call.
-			console.log(data);
+			console.log(data.opendata);	
+			for (i=0; i <data.opendata.answer.data.station.length; i++){
+				console.log(data.opendata.answer.data.station[i]);
+			}	
 		},
 		error: function(){
 			
@@ -86,3 +109,4 @@ function getData(url){
 		}
 	});
 }
+
