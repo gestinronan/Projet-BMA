545ae2b9c2712b4c6a17a27e438f327ac68c3c51
gestinronan <gestin.ronan@gmail.com>
2012-11-08 17:18:42 +0100

    interface fini+recuperation time picker

--- android/src/com/example/cybel_bma/FirstUserView.java
+++ android/src/com/example/cybel_bma/FirstUserView.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.widget.TimePicker;
 
 
 public class FirstUserView extends Activity{
 
+@SuppressWarnings("null")
 public void onCreate(Bundle savedInstanceState){
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.firstuserview);
+	
+	TimePicker hourPicker= (TimePicker) findViewById(R.id.datePicker1);
+	hourPicker.setIs24HourView(true);
+	int hour=hourPicker.getCurrentHour();
+	int minut=hourPicker.getCurrentMinute();
+	
 		
 		
 		
