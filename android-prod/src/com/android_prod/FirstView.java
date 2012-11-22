35817ce040e214a9ee84a34ff6bc04d51ecb100e
gestinronan <gestin.ronan@gmail.com>
2012-11-22 22:14:34 +0100

    ajout d'une classe et mise ne place du thread d'attente

--- android-prod/src/com/android_prod/FirstView.java
+++ android-prod/src/com/android_prod/FirstView.java
@@ -1,35 +1,32 @@
 package com.android_prod;
 
-import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 
 public class FirstView extends Activity {
 	
-	/************** This is the local Variable declaration *******************/
-	
-	/*************************************************************************/
+	 protected int _ScreenTime = 10000;
 
-	/************* This is the first Class call when the app start **********/
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_first_view);
-	}
-	/*************************************************************************/
-	
-	
-	/********* This create the Action. For this View we don't need it*******/
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_first_view, menu);
-		
-		// For the first view, we don't need the Action Bar so we hide it
-		ActionBar actionBar = getActionBar();
-		actionBar.hide();
-		
-		return true;
-	}
-	/*************************************************************************/
+	 /** Called when the activity is first created. */
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.activity_first_view);
+	     // thread for displaying the SplashScreen
+	     Thread splashTread = new Thread() {
+	 
+	         public void run() {
+	             
+	             
+	                finish();
+
+	                Intent BMA =new Intent(getApplicationContext(),com.android_prod.MapView.class );
+	                 startActivity(BMA);
+	             
+	         }
+	     };
+	     splashTread.start();
+	 }
 	
 }
---
+++ android-prod/src/com/android_prod/MapView.java
@@ -0,0 +1,11 @@
+package com.android_prod;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MapView extends Activity {
+
+
+
+}
