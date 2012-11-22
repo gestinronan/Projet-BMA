26fbeed37f0bb9610adfe41404ac74ac237a1748
glefloch <glfloch@gmail.com>
2012-11-22 18:29:17 +0100

    First view editing (Action Bar hide), Permission added to the manifest

--- android-prod/src/com/android_prod/FirstView.java
+++ android-prod/src/com/android_prod/FirstView.java
@@ -5,18 +5,30 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class FirstView extends Activity {
+	
+	/************** This is the local Variable declaration *******************/
+	
+	/*************************************************************************/
 
-	@Override
+	/************* This is the first Class call when the app start **********/
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first_view);
 	}
-
-	@Override
+	/*************************************************************************/
+	
+	
+	/********* This create the Action. For this View we don't need it*******/
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_first_view, menu);
+		
+		// For the first view, we don't need the Action Bar so we hide it
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		
 		return true;
 	}
-
+	/*************************************************************************/
+	
 }
