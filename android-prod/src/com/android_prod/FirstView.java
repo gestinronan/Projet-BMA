125cf2641c1614f93c0b9def44d1c3f6e3fed1aa
gestinronan <gestin.ronan@gmail.com>
2012-11-23 11:14:00 +0100

    add HttpRequest and make asychon task at beginning

--- android-prod/src/com/android_prod/FirstView.java
+++ android-prod/src/com/android_prod/FirstView.java
@@ -1,32 +1,33 @@
 package com.android_prod;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class FirstView extends Activity {
 	
-	 protected int _ScreenTime = 10000;
+	 private Context context;
 
 	 /** Called when the activity is first created. */
 	 @Override
 	 public void onCreate(Bundle savedInstanceState) {
 	     super.onCreate(savedInstanceState);
 	     setContentView(R.layout.activity_first_view);
-	     // thread for displaying the SplashScreen
-	     Thread splashTread = new Thread() {
-	 
-	         public void run() {
-	             
-	             
-	                finish();
-
-	                Intent BMA =new Intent(getApplicationContext(),com.android_prod.MapView.class );
-	                 startActivity(BMA);
-	             
-	         }
-	     };
-	     splashTread.start();
+	
+			//
+			context = this;
+			
+			// Create an intent
+			Intent intent = new Intent(FirstView.this, MapViewClass.class); 
+			
+			
+			
+			// Create an object of the call HttpRequest
+		HttpRequestClass request = new HttpRequestClass(context, intent);
+		request.execute();
+	     
+	     
 	 }
 	
 }
