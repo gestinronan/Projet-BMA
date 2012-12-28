2f2bdc99cc011fe7073a1b5b5635b116dfcef86c
glefloch <glfloch@gmail.com>
2012-12-28 17:33:47 +0100

    graph scripts

--- BmaServeur/One_time_neo4j/CreateGraph.js
+++ BmaServeur/One_time_neo4j/CreateGraph.js
@@ -10,10 +10,14 @@
 
 // Connection mySql DataBase  
 var mysql      = require('mysql');
-var connection = mysql.createConnection('mysql://guillaume:guillaume@127.0.0.1:3306/test?debug=true');
+var connection = mysql.createConnection('mysql://guillaume:guillaume@127.0.0.1:3306/test?debug=false');
 
 // Connection Neo4j database
-var graph = new neo4j.GraphDatabase("http://localhost:7474");
+var neo4j = require('node-neo4j');
+db = new neo4j('http://localhost:7474');
+
+// Variable to get asyncronous function synchonous
+var async = require('async');
 
 // Variable of node
 var BikeNode = new Array();
@@ -21,10 +25,20 @@ var BusNode = new Array();
 var MetroNode = new Array();
 var TrainNode = new Array();
 
+
+// Variable
+var bikeNodeDone = false;
+var busNodeDone = false;
+var metroNodeDone = false;
+
 // Then we create all the node
 
+
+
+
 // Create nodes for bike Stops
-connection.query('SELECT * FROM Bike_Stops', function(err, result){
+function createBikeNode(){
+var query = connection.query('SELECT * FROM BikeStops', function(err, result){
 		
 	// Case there is an error
 	if(err){
@@ -38,15 +52,30 @@ connection.query('SELECT * FROM Bike_Stops', function(err, result){
 		for(i=0; i < result.length; i++){
 			
 			// Create the node
-			 BikeNode[i] = graph.node({"Name": result[i].BikeStop_name, "id": result[i].BikeStop_id, "Lat": result[i].BikeStop_lat, "Lng": result[i].BikeStop_lon});
+			 BikeNode[i] = db.insertNode({Name: result[i].BikeStop_name, id: result[i].BikeStop_id, Lat: result[i].BikeStop_lat, Lng: result[i].BikeStop_lon, type: "Bike"}, 
+			 	
+			 	// Callback (function which callend when the insertNode function is done)
+			 	function(err, node){
+
+			 		// Case of error
+			 		if(err){
+			 			console.log("An error occured");
+			 		} else
+			 		console.log("BikeNode created : " + node.id);
+			 	});
+			 if(i == result.length){
+			 	bikeNodeDone = true;
+			 }
 		}
 		
 	}	
 	
 });
+}
 
 // Create nodes for bus Stops
-connection.query('SELECT * FROM BusStops', function(err, result){
+function createBusNode(){
+var query = connection.query('SELECT * FROM BusStops', function(err, result){
 	
 	// Case there is an error
 	if(err){
@@ -60,19 +89,36 @@ connection.query('SELECT * FROM BusStops', function(err, result){
 		for(i=0; i < result.length; i++){
 			
 			// Create the node
-			 BusNode[i] = graph.node({"Name": result[i].Stop_name, "id": result[i].Stop_id, "Lat": result[i].Stop_lat, "Lng": result[i].Stop_lon});
+			 BusNode[i] = db.insertNode({Name: result[i].Stop_name, id: result[i].Stop_id, Lat: result[i].Stop_lat, Lng: result[i].Stop_lon, type:"Bus"},
+			 
+			 	// Callback (function which callend when the insertNode function is done)
+			 	function(err, node){
+
+			 		// Case of error
+			 		if(err){
+			 			console.log("An error occured");
+			 		} else
+			 		console.log("BusNode created : " + node.id);
+			 	});
+			 if (i == result.length) {
+			 	busNodeDone = true;
+			 };
+
 		}
 		
 	}	
 	
 });
+}
 
 // Create nodes for Metro Stops
-connection.query('SELECT * FROM Metro_Stops', function(err, result){
+
+function createMetroNode(){
+var query = connection.query('SELECT * FROM MetroStops', function(err, result){
 
 	// Case there is an error
 	if(err){
-		console.log("An error Occured during the creation of MetroStop nodes");
+		console.log("An error Occured during the creation of MetroStops nodes");
 	} 
 	
 	// Case of success
@@ -82,10 +128,23 @@ connection.query('SELECT * FROM Metro_Stops', function(err, result){
 		for(i=0; i < result.length; i++){
 			
 			// Create the node
-			 MetroNode[i] = graph.node({"Name": result[i].MetroStop_name, "id": result[i].MetroStop_id, "Lat": result[i].MetroStop_lat, "Lng": result[i].MetroStop_lon});
+			 MetroNode[i] = db.insertNode({Name: result[i].MetroStop_name, id: result[i].MetroStop_id, Lat: result[i].MetroStop_lat, Lng: result[i].MetroStop_lon, type:"Metro"},
+			 	
+			 	// Callback (function which callend when the insertNode function is done)
+			 	function(err, node){
+
+			 		// Case of error
+			 		if(err){
+			 			console.log("An error occured");
+			 		} else
+			 		console.log("MetroNode created : " + node.id);
+			 	});
+			 if(i == result.length){
+			 	metroNodeDone = true;
+			 }
 		}
 		
 	}		
 });
-
+}
 
