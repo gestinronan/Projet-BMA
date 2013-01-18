2191a828ca782f29e78fa0c7064968053f98ab56
glefloch <glfloch@gmail.com>
2013-01-18 20:43:54 +0100

    Borne electrique database script

---
+++ BmaServeur/One_time_sql/StoreBorneElec.js
@@ -0,0 +1,105 @@
+/******************
+* filename: StoreBorneElec.js
+* data: 05/12/12
+* author: Guillaume Le Floch
+* version: 1.0
+******************/
+
+///////////////////////////////////////////////////////////////////////////
+////// This Script add each electric borne to the database /////////
+///////////////////////////////////////////////////////////////////////////
+
+// Variables needed for the database connection
+var mysql      = require('mysql');
+var connection = mysql.createConnection('mysql://guillaume:guillaume@127.0.0.1:3306/test?debug=false');
+
+// Connection to the database
+connection.connect(function(err){
+	
+	// Case there is an error during the connection
+	if(err){
+		console.log("Connection problem : " + err);
+	} else
+	console.log("Connection ok");	
+});
+
+// Jquery variable
+var $ = require('jquery');
+
+// Useful to load the file
+var fs = require('fs');
+
+// Path to GTFS file containing the borne electrique file
+var pathToData = "../Data_BorneElectrique/borne_vehicule_electrique.txt";
+
+// Global Variable
+var allTextLines = null;
+var headers = null;
+
+
+// Load the text file
+fs.readFile(pathToData, function (err, data) {
+  
+	// Case there is an error
+  	if (err) {
+	 	 console.log("File not load");
+ 	}
+	 
+
+	// Function that read the file and save the data 
+	processData(data.toString());
+});
+
+
+/**
+* This function parse the text file and save the data to the database
+* @Parameter:
+* Data: String that contain the data
+*/
+
+function processData(data){
+    var allTextLines = data.split(/\r\n|\n/);
+    
+	// Get the header
+	var headers = allTextLines[0].split(',');
+		
+	// Parse the data except the first line (header)
+    for (var i=1; i<allTextLines.length; i++) {
+        
+		// Split the line
+		var data = allTextLines[i].split(';');
+        if (data.length == headers.length) {
+			
+			// Get the data to save
+			var id = data[0];
+			var name = data[1];
+			var site_type = data[2];
+			var access = data[3];
+			var tarif = data[5];
+			var borne_type = data[6];
+			var latitude = data[17];
+			var longitude = data[16];
+				
+			// Save the data into the BusStops table
+			var query = connection.query("INSERT INTO test.BorneElec SET ?", {id: id, name: name, site_type: site_type, access: access, tarif: tarif, borne_type: borne_type, latitude: latitude, longitude: longitude}, function(err, result) {
+			  	
+					// Case there is an error
+					if(err || !data){
+						console.log("An error occured: " + err);
+					} else {
+						console.log("Bus route saved");
+					}
+				});
+			//console.log(query);	
+		}
+	
+}
+}
+
+
+
+
+
+
+
+
