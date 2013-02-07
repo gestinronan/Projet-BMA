ee64007262518bb1f3c5b716dfc04b4a282ed7c9
glefloch <glfloch@gmail.com>
2013-02-07 08:51:17 +0100

    test cypher shortest path working

--- BmaServeur/One_time_neo4j/RelationCreation/CreateBusRelation.js
+++ BmaServeur/One_time_neo4j/RelationCreation/CreateBusRelation.js
@@ -19,32 +19,157 @@ db = new neo4j('http://localhost:7474');
 // Variable needed to use jquery
 var $ = require('jquery');
 
+/**
+* Get all the trip_id from the sql table
+*/
+connection.query('SELECT Trip_id FROM test.BusTrips', function(err, result){
+
+  // Case there is an error during the request
+  if(err || !result){
+    console.log('An error occured getting the sql trip_id :: ' + err);
+  } else {
+    console.log('Trips_id stored');
+    getDataForATripId(result); // Call the function which will parse the data
+  }
+});
+
+/**
+* Parse the trip ID 
+*/
+function getDataForATripId(trips){
+
+  // Parse the all tripsId
+  for(i=0; i<trips.length; i++){
+
+    /* For each trip id, we query the database to get all the stop_id, arrival and departure time and stop sequence
+     The SQL query is the following one:
+
+     SELECT BusTrips.Trip_id, BusStop_times.*, BusTrips.Trip_headsign, BusStops.Stop_id
+     FROM BusStops as busstops, BusTrips as bustrips, BusStop_times as busstop_times
+     WHERE bustrips.Trip_id = '"1"'
+     AND bustrips.Trip_id = busstop_times.Trip_id
+     AND busstop_times.Stop_id = busstops.Stop_id
+     ORDER BY busStop_times.Stop_sequence ASC;
+     */
+
+     connection.query("SELECT BusTrips.Trip_id, BusStop_times.*, BusTrips.Trip_headsign, BusStops.Stop_id, BusStops.NodeId " +
+       "FROM BusStops as busstops, BusTrips as bustrips, BusStop_times as busstop_times " +
+       "WHERE bustrips.Trip_id = \'" + trips[i].Trip_id + "\' " +
+       "AND bustrips.Trip_id = busstop_times.Trip_id " +
+       "AND busstop_times.Stop_id = busstops.Stop_id " +
+       "ORDER BY busStop_times.Stop_sequence ASC", function(err, result){
+
+                      // Case of error getting the data concerning the trip_id
+                      if(err || !result){
+                        console.log('An error occured getting the data from the trip_id :: ' + trips[i].Trip_id + ' :: ' + err);
+                      } else {
+                        parseDataOfTrip(result); // Call the function which parse the data
+                      }
+
+                    });
+   }
+
+ }
+
+/**
+* This function will parse the data in order to create relations between busStops
+*/
+function parseDataOfTrip(data){
+
+  // Variable of the function
+  
+  var temp = null
+  var time = null;
+  var departTime = null;
+  var arrivalTime = null
+
+  // Parse the data
+  for(var j=0; j< data.length; j++){
+
+    var time = null ;  // Initialize the variable
+
+    // Check if there is a next stop
+    if(j+1 <data.length -1){
 
 
+     // First extract the departure Time of the first stops
+     temp = data[j].Departure_time.split(':');
+
+     // Convert the departure time into Seconds
+     departTime = temp[0]*3600 + temp[1] * 60 + temp[2];
+
+     // Get the arrival time of the next Stops
+     temp = data[j+1].Arrival_time.split(':');
+
+     // Convert the arrival time into seconds
+     arrivalTime = temp[0]*3600 + temp[1] * 60 + temp[2];
+
+     // Get the difference between the two times
+     time = arrivalTime - departTime;
+     time = time / 60; // Convert seconds into minute
+
+     /*** Debug ****/
+
+     console.log('Depart Time :: ' + data[j].Departure_time);
+     console.log('Arrival Time :: ' + data[j+1].Arrival_time);
+     console.log('Time Between :: ' + time);
+
+     // Create the relation
+     db.insertRelationship(data[j].NodeId, data[j+1].NodeId, 'Bus', {
+      time: time,
+      trip_id: data[j].Trip_id,
+      trip_headsign: data[j].Trip_headsign
+     }, function(err, result){
+
+      // Case of error
+      if(err || !result){
+        console.log('An error occured creatingthe relation :: ' + err);
+      } else {
+        console.log('Relation created :: ' + result.id);
+      }
+
+     });
+
+     // Then we create the relation between thos bus Stops
+     //createRelation(data[0].Trip_id, data[0].Trip_headsign, time, data[j].NodeId, data[j+1].NodeId, 'Bus');
+  }
+
+}
+}
 
 /**
 * This function add the relation into the graph 
 */
 
-function createRelation(dist, time, pointA, pointB, type){
+function createRelation(trip_id, trip_headsign, time, pointA, pointB, type){
 
   console.log('Create the relation');
-  db.insertRelationship(pointA.NodeId, pointB.NodeId, type, {
-    distance: dist,
-    time: time
-   }, function(err, relationship){
-        if(err) throw err;
+
+  /**** Debug ****/
+  console.log('trip_id :: ' + trip_id);
+  console.log('trip_headsign :: ' + trip_headsign);
+  console.log('time :: ' + time);
+  console.log('pointA :: ' + pointA);
+  console.log('pointB :: ' + pointB);
+  console.log('type :: ' + type);
+
+  db.insertRelationship(pointA, pointB, type, {
+    time: time,
+    trip_id: trip_id,
+    trip_headsign: trip_headsign
+  }, function(err, relationship){
+    if(err) console.log(err);
 
         // Output relationship properties.
-        console.log(relationship.data);
+        console.log('data :: ' + relationship.data);
 
         // Output relationship id.
-        console.log(relationship.id);
+        console.log('realtionship id :: ' + relationship.id);
 
         // Output relationship start_node_id.
-        console.log(relationship.start_node_id);
+        console.log('start node id :: ' + relationship.start_node_id);
 
         // Output relationship end_node_id.
-        console.log(relationship.end_node_id);
-  });
+        console.log('end node id :: ' + relationship.end_node_id);
+      });
 }
--- BmaServeur/One_time_neo4j/testCypherQuery.js
+++ BmaServeur/One_time_neo4j/testCypherQuery.js
@@ -19,16 +19,23 @@ db = new neo4j('http://localhost:7474');
 // Variable needed to use jquery
 var $ = require('jquery');
 
+// Global variable
+
+var nodes = new Array();
+var relations = new Array();
+var relationParameter = new Array();
+var nodeParameter = new Array();
+
 /** Cypher query which will be executed 
-* 	
+ 	
 	START d=node(47), e=node(98)
 	MATCH p = allShortestPaths( d-[*..15]->e )
 	RETURN p;
-***************************************/
+*/
 
 db.cypherQuery("START d=node(47), e=node(98) " +
-					  				   "MATCH p = allShortestPaths( d-[*..50]->e ) " +
-                       				   "RETURN p", function(err,result){
+			   "MATCH p = allShortestPaths( d-[*..50]->e ) " +
+               "RETURN p", function(err,result){
 			   	// Case of error 
 			   	if(err || !result){
 			   		console.log("An error Occured :: " + err);
@@ -37,11 +44,111 @@ db.cypherQuery("START d=node(47), e=node(98) " +
 
 			   	// Else Display the data into the console
 			   	else {
-			   		console.log(result);
 
-			   		console.log(result.data[0].nodes);
-			   		console.log(result.data[0].relationships);
+			   		// Read the data
+			   		nodes = result.data[0].nodes;
+			   		relations = result.data[0].relationships;
+
+			   		// Call the method to get the relation parameters
+			   		readRelationship(relations);
 			   	
 			   	}
 
 });
+
+/**
+* This function read relation ship
+*/
+function readRelationship(relations){
+	var i=0;
+	var x =0;
+	// Parse all the relation 
+	for(i = 0; i<relations.length; i++){
+
+		// Split the realtion into an array
+		var temp = relations[i].split('/');
+
+		// Extract the ID
+		var idRelation = temp[temp.length -1 ];
+
+		// Read the relation 
+		db.readRelationship(idRelation, function(err, result){
+
+			// Case an error occured reading the realtionship
+			if(err || !result){
+				console.log('An error occured getting relationship parameters :: ' + err );
+			} else {
+				
+				relationParameter[x] = result; // Store the data
+				// If it's done, we call the next function which will read all the nodes
+				if(x == relations.length -1){
+					
+					readNode(nodes);
+				}
+				x ++ ;
+			}
+		});
+	}
+}
+
+/**
+* This function read nodes
+*/
+function readNode(nodes){
+	var j =0;
+	var y =0;
+
+	// Parse all the nodes
+	for(j=0; j<nodes.length; j ++){
+
+		// Split the node into an array
+		var temp = nodes[j].split('/');
+
+		// Extracr the ID
+		var idNode = temp[temp.length - 1];
+
+	
+
+		// Read the node
+		db.readNode(idNode, function(err, result){
+
+			// Case an error occured getting the node parameter
+			if(err || !result){
+				console.log('An error occured getting node paramaters :: ' + err);
+			} else {
+
+				nodeParameter[y] = result;
+
+				// If it's done, we display the result
+				if(y == nodes.length -1){
+					displayResult();
+				}
+				y++;
+			}
+		});
+	}
+}
+
+
+/**
+* This function is used to display the result
+*/
+function displayResult(){
+
+	//console.log(nodeParameter[0].data.type);
+	//console.log(nodeParameter[1].data.Name);
+
+		
+	// We parse all the node Id
+	for(var i = 0; i< nodeParameter.length; i++){
+
+		// display the node
+		console.log('Etape ' + i + ' Arret de :: ' + nodeParameter[i].data.type + ' name :: ' + nodeParameter[i].data.Name);
+		
+		// Display the relation
+		if(relationParameter.length > i ){
+			console.log('Trajet ' + i + ' de type :: ' + relationParameter[i].type + ' pour une durée de :: ' + relationParameter[i].data.time);
+		}
+	}
+
+}
