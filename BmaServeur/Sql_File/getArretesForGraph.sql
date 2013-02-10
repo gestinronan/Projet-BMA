147e82b1a693673f5c4b95aeba5b42cdf656c45e
glefloch <glfloch@gmail.com>
2013-02-10 11:23:00 +0100

    bus relation

--- BmaServeur/Sql_File/getArretesForGraph.sql
+++ BmaServeur/Sql_File/getArretesForGraph.sql
@@ -1,6 +1,6 @@
 SELECT BusTrips.Trip_id, BusStop_times.*, BusStops.Stop_id
 FROM BusStops as busstops, BusTrips as bustrips, BusStop_times as busstop_times
-WHERE bustrips.Trip_id = '"1"'
+WHERE bustrips.Trip_id = '"100"'
 AND bustrips.Trip_id = busstop_times.Trip_id
 AND busstop_times.Stop_id = busstops.Stop_id
 ORDER BY busStop_times.Stop_sequence ASC;
