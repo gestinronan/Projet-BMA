3a97ca96e349d17d344b849a95a9e516f96183f9
glefloch <glfloch@gmail.com>
2013-02-20 12:34:46 +0100

    app changes

--- android-prod/src/com/android_prod/RoadRequestReciver.java
+++ android-prod/src/com/android_prod/RoadRequestReciver.java
@@ -46,12 +46,16 @@ public class RoadRequestReciver extends BroadcastReceiver {
 		try {
 			JSONObject test = new JSONObject(road.getStringExtra("road"));
 			roadArray = test.getJSONArray("relations");
+			
 
 			JSONObject tmpRela;
 
 			for (int i = 0; i < roadArray.length(); i++) {
 				tmpRela=roadArray.getJSONObject(i);
-
+				
+				// DEBUG
+				System.out.println(tmpRela);
+				
 				GeoRoad.add(MapViewClass.roadtrip.get(tmpRela.getString("Start_Stop_id")));			// Get the start point
 				GeoRoad.add(MapViewClass.roadtrip.get(tmpRela.getString("End_Stop_id"))); 			// Get the endpoint
 				//GeoRoad.add(MapViewClass.roadtrip.get(tmpRela.getString("type")));					// Get the type 
@@ -60,28 +64,36 @@ public class RoadRequestReciver extends BroadcastReceiver {
 				Object [] param = new Object[1];
 				param[0] = GeoRoad;
 				
-				// appel asycnhrone de creation des route 
+				// appel asycnhrone de creation des routes 
 				testroad.execute(param);
 				
 				Road road1;
 
 				road1 = (Road) testroad.get();
 
-
-				PathOverlay roadOverlay = RoadManager.buildRoadOverlay(road1, MapViewClass.mapView.getContext());
+			
+			//	PathOverlay roadOverlay = RoadManager.buildRoadOverlay(road1, MapViewClass.mapView.getContext());
 				
 				// Set the road color depending of the type
 				if(tmpRela.getString("type").equals("Bike")){
-					roadOverlay.setColor(Color.GREEN);
+					PathOverlay	roadOverlay = new PathOverlay(Color.GREEN, MapViewClass.mapView.getContext());
+					roadOverlay = RoadManager.buildRoadOverlay(road1, MapViewClass.mapView.getContext());
+					roadList.add(roadOverlay);	
 				} else if(tmpRela.getString("type").equals("Metro")){
-					roadOverlay.setColor(Color.RED);
+					PathOverlay roadOverlay = new PathOverlay(Color.RED, MapViewClass.mapView.getContext());
+					roadOverlay = RoadManager.buildRoadOverlay(road1, MapViewClass.mapView.getContext());
+					roadList.add(roadOverlay);
 				} else if(tmpRela.getString("type").equals("Foot")){
-					roadOverlay.setColor(Color.YELLOW);
+					PathOverlay roadOverlay = new PathOverlay(Color.YELLOW, MapViewClass.mapView.getContext());
+					roadOverlay = RoadManager.buildRoadOverlay(road1, MapViewClass.mapView.getContext());
+					roadList.add(roadOverlay);
 				} else if(tmpRela.getString("type").equals("Bus")){
-					roadOverlay.setColor(Color.BLUE);
+					PathOverlay roadOverlay = new PathOverlay(Color.BLUE, MapViewClass.mapView.getContext());
+					roadOverlay = RoadManager.buildRoadOverlay(road1, MapViewClass.mapView.getContext());
+					roadList.add(roadOverlay);
 				}
 				
-				roadList.add(roadOverlay);
+				
 
 
 			}
